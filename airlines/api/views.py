b5c9c2c3d3b5fd313a6362b154fffd2d7026879b
ibrahimardic <ibrahimard2000@gmail.com>
2024-10-19 23:45:47 +0300

    Add CRUD operations for Airline and Aircraft models

--- airlines/api/views.py
+++ airlines/api/views.py
@@ -1,29 +1,83 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from airlines.models import Airline, Aircraft
 from airlines.api.serializers import AirlineSerializer, AircraftSerializer
+from django.shortcuts import get_object_or_404
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def AirlinesView(request):
-    airlines = Airline.objects.all()
-    serializer = AirlineSerializer(airlines, many=True)  # Ensure `many=True` for QuerySet serialization
-    return Response(serializer.data, status=200)
 
-@api_view(['GET'])
+    if request.method == 'GET':
+        airlines = Airline.objects.all()
+        serializer = AirlineSerializer(airlines, many=True)  # Ensure `many=True` for QuerySet serialization
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = AirlineSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # IF not valid
+
+@api_view(['GET', 'POST'])
 def AirCraftsView(request):
-    aircrafts = Aircraft.objects.all()
-    serializer = AircraftSerializer(aircrafts, many=True)
-    return Response(serializer.data, status=200)
+    if request.method == 'GET':
+        aircrafts = Aircraft.objects.all()
+        serializer = AircraftSerializer(aircrafts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = AircraftSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # IF not valid    
 
-@api_view(['GET'])
+@api_view(['GET','DELETE', 'PATCH'])
 def RetrieveAircraftView(request, pk):
-    aircraftObj = Aircraft.objects.get(id=pk)
-    serializer = AircraftSerializer(aircraftObj)
-    return Response(serializer.data, status=200)
 
-@api_view(['GET'])
+    aircraft = get_object_or_404(Aircraft, id=pk)
+
+    if request.method == 'GET':
+        serializer = AircraftSerializer(aircraft)
+        return Response(serializer.data, status= status.HTTP_200_OK)
+    
+    elif request.method == 'PATCH':
+        serializer = AircraftSerializer(aircraft, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # IF not valid
+    
+    elif request.method == 'DELETE':
+        aircraft.delete()
+        return Response(status=status.HTTP_200_OK)  # No content, no data returned
+
+
+@api_view(['GET', 'PATCH', 'DELETE'])
 def RetrieveAirlineView(request,pk):
-    airlineObj = Airline.objects.get(id=pk)
-    serializer = AirlineSerializer(airlineObj)
-    return Response(serializer.data, status=200)
+    airline = get_object_or_404(Airline, id=pk)
+
+    if request.method == 'GET':
+
+        serializer = AirlineSerializer(airline)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PATCH':
+        
+        serializer = AirlineSerializer(airline, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # IF not valid
+    
+    elif request.method == 'DELETE':
+        airline.delete()
+
+        return Response(status=status.HTTP_200_OK) 
