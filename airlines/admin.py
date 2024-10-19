02395bba9e01731d71175ecead0f28e800e7b1bd
ibrahimardic <ibrahimard2000@gmail.com>
2024-10-19 21:38:37 +0300

    add serializers and update views to return serialized data

--- airlines/admin.py
+++ airlines/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Airline, Aircraft
 
-# Register your models here.
+admin.site.register(Airline)
+admin.site.register(Aircraft)
--- airlines/api/views.py
+++ airlines/api/views.py
@@ -1,23 +1,29 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from airlines.models import Airline, Aircraft
+from airlines.api.serializers import AirlineSerializer, AircraftSerializer
 
 
 @api_view(['GET'])
 def AirlinesView(request):
-
-    return Response({"message": "airlines"},status=200) 
+    airlines = Airline.objects.all()
+    serializer = AirlineSerializer(airlines, many=True)  # Ensure `many=True` for QuerySet serialization
+    return Response(serializer.data, status=200)
 
 @api_view(['GET'])
 def AirCraftsView(request):
-
-    return Response({"message": "aircrafts"}, status=200)
+    aircrafts = Aircraft.objects.all()
+    serializer = AircraftSerializer(aircrafts, many=True)
+    return Response(serializer.data, status=200)
 
 @api_view(['GET'])
 def RetrieveAircraftView(request, pk):
-
-    return Response(status=200)
+    aircraftObj = Aircraft.objects.get(id=pk)
+    serializer = AircraftSerializer(aircraftObj)
+    return Response(serializer.data, status=200)
 
 @api_view(['GET'])
-def RetrieveAirlineView(request,ok):
-
-    return Response(status=200)
+def RetrieveAirlineView(request,pk):
+    airlineObj = Airline.objects.get(id=pk)
+    serializer = AirlineSerializer(airlineObj)
+    return Response(serializer.data, status=200)
