772efc0bc5c2d8ded6bc1344b330cf938b6f5b8b
ibrahimardic <ibrahimard2000@gmail.com>
2024-10-26 00:44:47 +0300

    Allowed hosts defined

--- AirlineManager/settings.py
+++ AirlineManager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,9 @@ SECRET_KEY = "django-insecure-pi)y=_ged4yo)ppm@x%uybf%m(c%$r353j0*#eb=k0+o1fboar
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+load_dotenv()
+
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
